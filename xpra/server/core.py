35327c1e08051a7185dceb92e076058f7defaa5f
Antoine Martin <antoine@xpra.org>
2026-05-28 15:01:04 +0700

    #4891 stop asyncio loop if it exists
    (cherry picked from commit d470a15503c15f1c477e841f6e95cf943534e9f3)

--- xpra/server/core.py
+++ xpra/server/core.py
@@ -53,7 +53,7 @@ from xpra.util.system import register_SIGUSR_signals, get_run_info
 from xpra.util.io import load_binary_file, find_libexec_command
 from xpra.util.background_worker import quit_worker
 from xpra.util.thread import start_thread, check_main_thread
-from xpra.common import noop, noerr, subsystem_name
+from xpra.common import noop, noerr, subsystem_name, stop_asyncio_loop
 from xpra.constants import DEFAULT_XDG_DATA_DIRS
 from xpra.util.pysystem import dump_all_frames
 from xpra.util.objects import typedict
@@ -330,6 +330,7 @@ class ServerCore(ServerBaseClass):
         self.cleanup_all_protocols()
         self.do_cleanup()
         self.cleanup_sockets()
+        stop_asyncio_loop()
         netlog("cleanup() done for server core")
 
     def do_cleanup(self) -> None:
